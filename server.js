475829d86706cd67aeb430e3aed52b4ed58eff34
Brucimus <Brucimus@gmail.com>
2018-08-18 11:17:19 -0700

    changed links in html

--- server.js
+++ server.js
@@ -7,10 +7,59 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 //reservations variable
-var reservations = [];
+var reservations = [
+    {
+        customerName: "yoda",
+        phoneNumber: 1234567890,
+        customerEmail: "yoda@yoda.com",
+        customerID: 900
+    },
+    {
+        customerName: "yoda",
+        phoneNumber: 1234567890,
+        customerEmail: "yoda@yoda.com",
+        customerID: 900
+    },
+    {
+        customerName: "yoda",
+        phoneNumber: 1234567890,
+        customerEmail: "yoda@yoda.com",
+        customerID: 900
+    },
+    {
+        customerName: "yoda",
+        phoneNumber: 1234567890,
+        customerEmail: "yoda@yoda.com",
+        customerID: 900
+    },
+    {
+        customerName: "yoda",
+        phoneNumber: 1234567890,
+        customerEmail: "yoda@yoda.com",
+        customerID: 900
+    },
+    {
+        customerName: "yoda",
+        phoneNumber: 1234567890,
+        customerEmail: "yoda@yoda.com",
+        customerID: 900
+    },
+    {
+        customerName: "yoda",
+        phoneNumber: 1234567890,
+        customerEmail: "yoda@yoda.com",
+        customerID: 900
+    },
+    {
+        customerName: "yoda",
+        phoneNumber: 1234567890,
+        customerEmail: "yoda@yoda.com",
+        customerID: 900
+    },
+];
 
 // Routes
 // =============================================================
@@ -26,6 +75,11 @@ app.get("/tables", function(req, res) {
 
 app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// Displays all characters
+app.get("/api/waitlist", function(req, res) {
+    return res.json(reservations);
 });
 
 // Starts the server to begin listening
